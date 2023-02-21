e1da97df19e485f4493ecdd5295b0df0392dc166
yassir <yas.esraidi@gmail.com>
2023-02-21 17:31:38 +0100

    done with album api crud

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -19,9 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::post('/test',function(){
-//     return 15;
-// })->middleware('checkManager');
+Route::post('/test', function () {
+    return 15;
+});
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -29,3 +30,5 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
 });
+
+Route::apiResource('/album', AlbumController::class)->middleware(['checkAdminManager']);
